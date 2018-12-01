a161efce178c0387b7e4e5149fbf37e35a035754
Drew Pearce <andrew.f.pearce@accenture.com>
2018-12-01 13:32:54 -0500

    forgot to add the text file name in day 1

--- day_01.py
+++ day_01.py
@@ -3,6 +3,6 @@ from funcs import challenge_01_2
 from funcs import get_inputs
 
 
-inputs = get_inputs()
+inputs = get_inputs('day_01.txt')
 print('Challenge 1 result: {}'.format(challenge_01_1(inputs)))
 print('Challenge 2 result: {}'.format(challenge_01_2(inputs)))
