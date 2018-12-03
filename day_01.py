700d291a6f903e598feb3d8d1e5a6991d134094c
Drew Pearce <andrew.f.pearce@accenture.com>
2018-12-03 18:17:30 -0500

    add spacing for readability

--- day_01.py
+++ day_01.py
@@ -6,9 +6,11 @@ from funcs import now_ms
 
 
 inputs = get_inputs('day_01.txt')
+
 start_ms = now_ms()
 print('Challenge 1 result: {}'.format(challenge_01_1(inputs)))
 log_runtime(start_ms)
+
 start_ms = now_ms()
 print('Challenge 2 result: {}'.format(challenge_01_2_redux(inputs)))
 log_runtime(start_ms)
--- day_02.py
+++ day_02.py
@@ -6,9 +6,11 @@ from funcs import now_ms
 
 
 inputs = get_inputs_str('day_02.txt')
+
 start_ms = now_ms()
 print('Challenge 1 result: {}'.format(challenge_02_1(inputs)))
 log_runtime(start_ms)
+
 start_ms = now_ms()
 print('Challenge 2 result: {}'.format(challenge_02_2(inputs)))
 log_runtime(start_ms)
